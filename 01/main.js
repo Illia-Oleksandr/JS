40abcc3ff2307b7b0629b75fd88274110fb3ce45
Illia-Oleksandr <illiaoleksandr.moskaliuk@gmail.com>
2024-03-05 07:10:58 +0200

    first commit

---
+++ 01/main.js
@@ -0,0 +1,8 @@
+//--------browser dialogues--------------
+alert("Hello js");
+let age = +prompt("Enter your age:");
+console.log(`Your age is ${age}`);
+confirm("Do you want to continue?");
+//--------interpolation-------------
+let email = "illiaoleksandr.moskaliuk@gmail.com"
+console.log(`email: ${email}, type: ${typeof (email)}`);
