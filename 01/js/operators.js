883cbc921cf7edf8b42213fa0fc8a826cc4c45be
Illia-Oleksandr <illiaoleksandr.moskaliuk@gmail.com>
2024-03-06 06:50:47 +0200

    operators example

---
+++ 01/js/operators.js
@@ -0,0 +1,10 @@
+//--------operators--------------
+//--------ariphmetic operators--------------
+
+console.log(`a + b: ${5 + 3}`);
+console.log(`a - b: ${5 - 3}`);
+console.log(`a * b: ${5 * 3}`);
+console.log(`a / b: ${5 / 3}`);
+console.log(`a % b: ${5 % 3}`);
+
+console.log(`a ** b: ${5 ** 3}`);
